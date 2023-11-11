765feefa15ba93d52588315fa536a6bd5a227a87
hieudtrung <nigel_xzzz@yahoo.com.vn>
2023-11-11 18:06:37 +0100

    + code template for database management

---
+++ be-dbms/pipelines/dm_cost_prediction.py
@@ -0,0 +1,6 @@
+"""ETL pipeline from data warehouse to data mart, used specifically to create datasets for training cost prediction models.
+These pipelines are hosted serverlessly on Azure Functions
+"""
+
+def azure_function():
+    ...
